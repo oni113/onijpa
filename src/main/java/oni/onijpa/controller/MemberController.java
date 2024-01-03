d9d854d47e2977088189fc3d85629636d1c67eff
oni113 <miskw99@gmail.com>
2024-01-03 15:22:09 +0900

    mvc example

--- src/main/java/oni/onijpa/controller/MemberController.java
+++ src/main/java/oni/onijpa/controller/MemberController.java
@@ -1,8 +1,13 @@
 package oni.onijpa.controller;
 
+import oni.onijpa.domain.Member;
+import oni.onijpa.dto.member.MemberForm;
 import oni.onijpa.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +17,26 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String signUpForm() {
+        return "/members/signUpForm";
+    }
+
+    @PostMapping("/members/new")
+    public String signUp(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setEmail(form.getEmail());
+        memberService.join(member);
+
+        return "redirect:/members";
+    }
+
+    @GetMapping("/members")
+    public String members(Model model) {
+        model.addAttribute("members", memberService.findMembers());
+        return "/members/memberList";
     }
 }
