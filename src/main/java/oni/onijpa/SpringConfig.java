9fd8770bfb166d04b0fd75c10f69b0a16e916171
oni113 <miskw99@gmail.com>
2024-01-19 16:55:12 +0900

    configure Spring-Data-JPA

--- src/main/java/oni/onijpa/SpringConfig.java
+++ src/main/java/oni/onijpa/SpringConfig.java
@@ -1,31 +1,21 @@
 package oni.onijpa;
 
-import jakarta.persistence.EntityManager;
-import oni.onijpa.repository.JpaMemberRepository;
 import oni.onijpa.repository.MemberRepository;
 import oni.onijpa.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+        return new MemberService(memberRepository);
     }
 }
